169dda90b24f47ff8c7ef1e5ddd6864011e69259
Zafir <55599023+zafir100100@users.noreply.github.com>
2024-05-28 23:27:16 +0600

    conditional input added for vital

--- cypress/e2e/SavePatientVitalTest.cy.js
+++ cypress/e2e/SavePatientVitalTest.cy.js
@@ -12,56 +12,56 @@ describe('Save patient vital information on https://staging-scweb.arcapps.org/',
     cy.viewport(1920, 1080);
   });
 
-  /**
-   * Test case for user login functionality.
-   */
-  describe('User Login', () => {
-    it('should log in successfully', () => {
-      const loginPage = new LoginPage();
-      loginPage.gotoLoginPage();
-      loginPage.doLogin('tester', 'tester2023!');
-    });
-  });
+  it('should log in, select facility, search for patient, navigate to vitals, and save vitals', () => {
+    // Login
+    const loginPage = new LoginPage();
+    loginPage.gotoLoginPage();
+    loginPage.doLogin('tester', 'tester2023!');
 
-  /**
-   * Test case for selecting the facility.
-   */
-  describe('Facility Selection', () => {
-    it('should select the facility', () => {
-      const facilitySelectionPage = new FacilitySelectionPage();
-      facilitySelectionPage.setFacility('Lusaka', 'Lusaka', 'Dr Watson Dental Clinic');
-    });
-  });
+    // Select Facility
+    const facilitySelectionPage = new FacilitySelectionPage();
+    facilitySelectionPage.setFacility('Lusaka', 'Lusaka', 'Dr Watson Dental Clinic');
 
-  /**
-   * Test case for searching a patient by NRC and attending to the patient.
-   */
-  describe('Patient Search', () => {
-    it('should search for a patient by NRC and attend to the patient', () => {
-      const patientSearchPage = new PatientSearchPage();
-      patientSearchPage.searchPatientByNrc('111111111');
-      patientSearchPage.attendToPatient();
-    });
-  });
+    // Search for Patient by NRC and Attend to Patient
+    const patientSearchPage = new PatientSearchPage();
+    patientSearchPage.searchPatientByNrc('111111111');
+    patientSearchPage.attendToPatient();
 
-  /**
-   * Test case for navigating to the vitals page.
-   */
-  describe('Service Point Navigation', () => {
-    it('should navigate to the vitals page', () => {
-      const servicePointPage = new ServicePointPage();
-      servicePointPage.clickVital();
-    });
-  });
+    // Navigate to Vitals Page
+    const servicePointPage = new ServicePointPage();
+    servicePointPage.clickVital();
 
-  /**
-   * Test case for adding and saving patient vitals.
-   */
-  describe('Add and Save Vitals', () => {
-    it('should add and save patient vitals', () => {
-      const vitalPage = new VitalPage();
-      vitalPage.clickAddVital();
-      vitalPage.saveVital(68, 168, 132, 67, 62, 19, null, null, null);
-    });
+    // Add and Save Patient Vitals
+    const vitalPage = new VitalPage();
+    vitalPage.clickAddVital();
+    vitalPage.saveVital(68, 168, 132, 67, 62, 19, null, null, null);
   });
+  
+  // it('should log in successfully', () => {
+  //   const loginPage = new LoginPage();
+  //   loginPage.gotoLoginPage();
+  //   loginPage.doLogin('tester', 'tester2023!');
+  // });
+
+  // it('should select the facility', () => {
+  //   const facilitySelectionPage = new FacilitySelectionPage();
+  //   facilitySelectionPage.setFacility('Lusaka', 'Lusaka', 'Dr Watson Dental Clinic');
+  // });
+
+  // it('should search for a patient by NRC and attend to the patient', () => {
+  //   const patientSearchPage = new PatientSearchPage();
+  //   patientSearchPage.searchPatientByNrc('111111111');
+  //   patientSearchPage.attendToPatient();
+  // });
+
+  // it('should navigate to the vitals page', () => {
+  //   const servicePointPage = new ServicePointPage();
+  //   servicePointPage.clickVital();
+  // });
+
+  // it('should add and save patient vitals', () => {
+  //   const vitalPage = new VitalPage();
+  //   vitalPage.clickAddVital();
+  //   vitalPage.saveVital(68, 168, 132, 67, 62, 19, null, null, null);
+  // });
 });
--- cypress/pages/VitalPage.js
+++ cypress/pages/VitalPage.js
@@ -2,129 +2,158 @@
  * Class representing the vital page.
  */
 class VitalPage {
+  /**
+   * Creates an instance of VitalPage.
+   */
+  constructor() {
     /**
-     * Creates an instance of VitalPage.
+     * @property {string} ADD_VITAL_BUTTON_XPATH - XPath for the add vital button.
      */
-    constructor() {
-      /**
-       * @property {string} ADD_VITAL_BUTTON_XPATH - XPath for the add vital button.
-       */
-      this.ADD_VITAL_BUTTON_XPATH = "//button[text()='Add Vital']";
-  
-      /**
-       * @property {string} WEIGHT_XPATH - XPath for the weight input field.
-       */
-      this.WEIGHT_XPATH = "//input[@type='text'][contains(@placeholder,'Weight')]";
-  
-      /**
-       * @property {string} HEIGHT_XPATH - XPath for the height input field.
-       */
-      this.HEIGHT_XPATH = "//input[@type='text'][contains(@placeholder,'Height')]";
-  
-      /**
-       * @property {string} SYSTOLIC_XPATH - XPath for the systolic input field.
-       */
-      this.SYSTOLIC_XPATH = "//input[@type='text'][contains(@placeholder,'Systolic')]";
-  
-      /**
-       * @property {string} DIASTOLIC_XPATH - XPath for the diastolic input field.
-       */
-      this.DIASTOLIC_XPATH = "//input[@type='text'][contains(@placeholder,'Diastolic')]";
-  
-      /**
-       * @property {string} PULSE_RATE_XPATH - XPath for the pulse rate input field.
-       */
-      this.PULSE_RATE_XPATH = "//input[@name='pulseRate']";
-  
-      /**
-       * @property {string} RESPIRATORY_RATE_XPATH - XPath for the respiratory rate input field.
-       */
-      this.RESPIRATORY_RATE_XPATH = "//input[@name='respiratoryRate']";
-  
-      /**
-       * @property {string} OXYGEN_SATURATION_XPATH - XPath for the oxygen saturation input field.
-       */
-      this.OXYGEN_SATURATION_XPATH = "//input[@name='oxygenSaturation']";
-  
-      /**
-       * @property {string} ABDOMINAL_CIRCUMFERENCE_XPATH - XPath for the abdominal circumference input field.
-       */
-      this.ABDOMINAL_CIRCUMFERENCE_XPATH = "//input[@name='abdominalCircumference']";
-  
-      /**
-       * @property {string} COMMENT_XPATH - XPath for the comment textarea.
-       */
-      this.COMMENT_XPATH = "//textarea[@name='comment']";
-  
-      /**
-       * @property {string} SAVE_BUTTON_XPATH - XPath for the save button.
-       */
-      this.SAVE_BUTTON_XPATH = "//button[@type='submit']/span[contains(text(),'Save')]";
-    }
-  
+    this.ADD_VITAL_BUTTON_XPATH = "//button[text()='Add Vital']";
+
     /**
-     * Clicks on the add vital button to navigate to the add vitals page.
+     * @property {string} WEIGHT_XPATH - XPath for the weight input field.
      */
-    clickAddVital() {
-      cy.xpath(this.ADD_VITAL_BUTTON_XPATH)
-        .should('be.visible')
-        .click();
-    }
-  
+    this.WEIGHT_XPATH = "//input[@type='text'][contains(@placeholder,'Weight')]";
+
     /**
-     * Fills out the vital form and saves the information.
-     * @param {string} weight - The weight of the patient.
-     * @param {string} height - The height of the patient.
-     * @param {string} systolic - The systolic blood pressure.
-     * @param {string} diastolic - The diastolic blood pressure.
-     * @param {string} pulseRate - The pulse rate.
-     * @param {string} respiratoryRate - The respiratory rate.
-     * @param {string} oxygenSaturation - The oxygen saturation.
-     * @param {string} abdominalCircumference - The abdominal circumference.
-     * @param {string} comment - Additional comments.
+     * @property {string} HEIGHT_XPATH - XPath for the height input field.
      */
-    saveVital(weight, height, systolic, diastolic, pulseRate, respiratoryRate, oxygenSaturation, abdominalCircumference, comment) {
+    this.HEIGHT_XPATH = "//input[@type='text'][contains(@placeholder,'Height')]";
+
+    /**
+     * @property {string} SYSTOLIC_XPATH - XPath for the systolic input field.
+     */
+    this.SYSTOLIC_XPATH = "//input[@type='text'][contains(@placeholder,'Systolic')]";
+
+    /**
+     * @property {string} DIASTOLIC_XPATH - XPath for the diastolic input field.
+     */
+    this.DIASTOLIC_XPATH = "//input[@type='text'][contains(@placeholder,'Diastolic')]";
+
+    /**
+     * @property {string} TEMPERATURE_XPATH - XPath for the temperature input field.
+     */
+    this.TEMPERATURE_XPATH = "//input[@name='temperature']";
+
+    /**
+     * @property {string} PULSE_RATE_XPATH - XPath for the pulse rate input field.
+     */
+    this.PULSE_RATE_XPATH = "//input[@name='pulseRate']";
+
+    /**
+     * @property {string} RESPIRATORY_RATE_XPATH - XPath for the respiratory rate input field.
+     */
+    this.RESPIRATORY_RATE_XPATH = "//input[@name='respiratoryRate']";
+
+    /**
+     * @property {string} OXYGEN_SATURATION_XPATH - XPath for the oxygen saturation input field.
+     */
+    this.OXYGEN_SATURATION_XPATH = "//input[@name='oxygenSaturation']";
+
+    /**
+     * @property {string} ABDOMINAL_CIRCUMFERENCE_XPATH - XPath for the abdominal circumference input field.
+     */
+    this.ABDOMINAL_CIRCUMFERENCE_XPATH = "//input[@name='abdominalCircumference']";
+
+    /**
+     * @property {string} COMMENT_XPATH - XPath for the comment textarea.
+     */
+    this.COMMENT_XPATH = "//textarea[@name='comment']";
+
+    /**
+     * @property {string} SAVE_BUTTON_XPATH - XPath for the save button.
+     */
+    this.SAVE_BUTTON_XPATH = "//button[@type='submit']/span[contains(text(),'Save')]";
+  }
+
+  /**
+   * Clicks on the add vital button to navigate to the add vitals page.
+   */
+  clickAddVital() {
+    cy.xpath(this.ADD_VITAL_BUTTON_XPATH)
+      .should('be.visible')
+      .click();
+  }
+
+  /**
+   * Fills out the vital form and saves the information.
+   * @param {string|number} weight - The weight of the patient.
+   * @param {string|number} height - The height of the patient.
+   * @param {string|number} systolic - The systolic blood pressure.
+   * @param {string|number} diastolic - The diastolic blood pressure.
+   * @param {string|number} temperature - The body temperature of the patient.
+   * @param {string|number} pulseRate - The pulse rate.
+   * @param {string|number} respiratoryRate - The respiratory rate.
+   * @param {string|number} oxygenSaturation - The oxygen saturation.
+   * @param {string|number} abdominalCircumference - The abdominal circumference.
+   * @param {string} comment - Additional comments.
+   */
+  saveVital(weight, height, systolic, diastolic, temperature, pulseRate, respiratoryRate, oxygenSaturation, abdominalCircumference, comment) {
+    if (weight !== null && weight !== undefined) {
       cy.xpath(this.WEIGHT_XPATH)
         .should('be.visible')
         .clear()
-        .type(weight);
+        .type(weight.toString());
+    }
+    if (height !== null && height !== undefined) {
       cy.xpath(this.HEIGHT_XPATH)
         .should('be.visible')
         .clear()
-        .type(height);
+        .type(height.toString());
+    }
+    if (systolic !== null && systolic !== undefined) {
       cy.xpath(this.SYSTOLIC_XPATH)
         .should('be.visible')
         .clear()
-        .type(systolic);
+        .type(systolic.toString());
+    }
+    if (diastolic !== null && diastolic !== undefined) {
       cy.xpath(this.DIASTOLIC_XPATH)
         .should('be.visible')
         .clear()
-        .type(diastolic);
+        .type(diastolic.toString());
+    }
+    if (temperature !== null && temperature !== undefined) {
+      cy.xpath(this.TEMPERATURE_XPATH)
+        .should('be.visible')
+        .clear()
+        .type(temperature.toString());
+    }
+    if (pulseRate !== null && pulseRate !== undefined) {
       cy.xpath(this.PULSE_RATE_XPATH)
         .should('be.visible')
         .clear()
-        .type(pulseRate);
+        .type(pulseRate.toString());
+    }
+    if (respiratoryRate !== null && respiratoryRate !== undefined) {
       cy.xpath(this.RESPIRATORY_RATE_XPATH)
         .should('be.visible')
         .clear()
-        .type(respiratoryRate);
+        .type(respiratoryRate.toString());
+    }
+    if (oxygenSaturation !== null && oxygenSaturation !== undefined) {
       cy.xpath(this.OXYGEN_SATURATION_XPATH)
         .should('be.visible')
         .clear()
-        .type(oxygenSaturation);
+        .type(oxygenSaturation.toString());
+    }
+    if (abdominalCircumference !== null && abdominalCircumference !== undefined) {
       cy.xpath(this.ABDOMINAL_CIRCUMFERENCE_XPATH)
         .should('be.visible')
         .clear()
-        .type(abdominalCircumference);
+        .type(abdominalCircumference.toString());
+    }
+    if (comment !== null && comment !== undefined) {
       cy.xpath(this.COMMENT_XPATH)
         .should('be.visible')
         .clear()
-        .type(comment);
-      cy.xpath(this.SAVE_BUTTON_XPATH)
-        .should('be.visible')
-        .click();
+        .type(comment.toString());
     }
+    cy.xpath(this.SAVE_BUTTON_XPATH)
+      .should('be.visible')
+      .click();
   }
-  
-  export default VitalPage;
-  
+}
+
+export default VitalPage;
